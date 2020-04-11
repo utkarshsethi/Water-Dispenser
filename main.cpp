6ac16d294260c1e58f963d9567aa677bbe941393
StorageB <44729718+StorageB@users.noreply.github.com>
2020-04-10 19:37:48 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+//  Automatic Water Dispenser
+//  https://github.com/StorageB/Water-Dispenser
+
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 #include <ESP8266WiFi.h>
@@ -5,18 +8,25 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-#define valve_output   D1   // valve output pin
-#define ir_input       D5   // ir sensor input piin
-#define switch_input   D7   // pushbutton switch input pin
-#define led_pin        D2   // NeoPixel strip signal pin
-#define led_count      8    // number of LEDs in NeoPixel strip
-#define led_brightness 200  // NeoPixel brightness (max = 255)
-#define pwmIntervals   100  // number of intervals in the fade in/out for loops
+#define valve_output   D1     // valve output pin
+#define ir_input       D5     // ir sensor input piin
+#define switch_input   D7     // pushbutton switch input pin
+#define led_pin        D2     // NeoPixel strip signal pin
+#define led_count      8      // number of LEDs in NeoPixel strip
+#define led_brightness 125    // NeoPixel brightness (max = 255)
+#define pwmIntervals   100    // number of intervals in the fade in/out for loops    
+#define error_time     60000  // amount of time valve can be open before automatically turning off and displaying an erro
 
-int ir_state;               // state of IR sensor - LOW if object detected, HIGH if no object detected
-int switch_state;           // state of pushbutton switch - HIGH if pressed, LOW if not pressed
-int light_on = 0;           // state of NeoPixel strip - 1 if on, 0 if off
-int ir_input_delay = 100;   // how long to wait once an object is detected by IR sensor - this prevents ghost inputs 
+int ir_state;                 // state of IR sensor - LOW if object detected, HIGH if no object detected
+int switch_state;             // state of pushbutton switch - HIGH if pressed, LOW if not pressed
+int light_on = 0;             // state of NeoPixel strip: 1 if on, 0 if off
+int valve_open = 0;           // state of the valve: 1 if open, 0 if closed 
+int ir_input_delay = 100;     // how long to wait once an object is detected by IR sensor - this prevents ghost inputs 
+int error_status = 0;         // set to 0 if no errors, 1 if there is an error
+
+unsigned long current_time = 0; // used to get current time
+unsigned long timer_start = 0;  // used to start timer when valve is open
+
 
 float R = (pwmIntervals * log10(2))/(log10(255)); // used to calculate the 'R' value for fading LEDs
 
@@ -25,11 +35,12 @@ Adafruit_NeoPixel strip(led_count, led_pin, NEO_GRB + NEO_KHZ800);
 
 // Required for OTA programming
 #ifndef STASSID
-#define STASSID "network name"  // enter network name
-#define STAPSK  "password"  	// enter password
+#define STASSID "ATTbR6Tspi"    // enter network name
+#define STAPSK  "jbkajycs%g29"  // enter password
 #endif
 const char* ssid = STASSID;
 const char* password = STAPSK;
+
 
 void setup() {
   Serial.begin(9600);
@@ -91,66 +102,108 @@ void setup() {
   
 
   // Setup
-  pinMode(LED_BUILTIN, OUTPUT);    // Initialize on board LED as output  
-  pinMode(valve_output, OUTPUT);   // Initialize pin as digital output   (solenoid valve)
-  pinMode(ir_input, INPUT);        // Initialize pin as digital input    (infrared sensor)
-  pinMode(switch_input, INPUT);    // Initialize pin as digital input    (pushbutton switch)
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize on board LED as output  
+  pinMode(valve_output, OUTPUT);    // Initialize pin as digital output   (solenoid valve)
+  pinMode(ir_input, INPUT);         // Initialize pin as digital input    (infrared sensor)
+  pinMode(switch_input, INPUT);     // Initialize pin as digital input    (pushbutton switch)
   
-  digitalWrite(LED_BUILTIN, HIGH); // LED off
-  digitalWrite(valve_output, LOW); // valve closed
+  digitalWrite(LED_BUILTIN, HIGH);  // LED off
+  digitalWrite(valve_output, LOW);  // valve closed
 
-  strip.begin();           		// INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            		// Turn OFF all pixels ASAP
-  strip.setBrightness(led_brightness); 	// Set BRIGHTNESS (max = 255)
+  strip.begin();                        // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();                         // Turn OFF all pixels ASAP
+  strip.setBrightness(led_brightness);  // Set BRIGHTNESS (max = 255)
 
 }
 
+
 // Fade LEDs on
-void fade_in(uint32_t color, int wait) {
+void fade_in(String fade_color, /*uint32_t color,*/ int wait) {
   int brightness = 0;
   for(int i=0; i<=pwmIntervals; i++){
     brightness = pow (2, (i / R)) - 1;
     for(int j=0; j<strip.numPixels(); j++) {
-      strip.setPixelColor(j,0,0,brightness);
+      if(fade_color=="blue") {
+        strip.setPixelColor(j,0,0,brightness);
+      }
+      if (fade_color=="red") {
+        strip.setPixelColor(j,brightness,0,0);
+      }
     }
     strip.show();
     delay(wait);
   }
-  light_on=1;
 }
 
 // Fade LEDs off
-void fade_out(uint32_t color, int wait) {
+void fade_out(String fade_color,/*uint32_t color, */int wait) {
   int brightness = 255;
   for(int i=pwmIntervals; i>=0; i--){
     brightness = pow (2, (i / R)) - 1;
     for(int j=0; j<strip.numPixels(); j++) {
-      strip.setPixelColor(j,0,0,brightness);
+      if(fade_color=="blue") {
+        strip.setPixelColor(j,0,0,brightness);
+      }
+      if (fade_color=="red") {
+        strip.setPixelColor(j,brightness,0,0);
+      }
     }
     strip.show();
     delay(wait);
   }
   strip.clear();
   strip.show();
-  light_on=0;
 }
 
-// Open valve and turn on LEDs
+// Turn off valve and display error on LEDs if an error is detected
+void error() {
+  digitalWrite(valve_output, LOW); // close valve
+  while (error_status == 1)
+  {
+    // uncomment to allow for OTA programming if stuck in this infinite loop, otherwise reset button must be pressed
+    // ArduinoOTA.handle(); 
+
+    fade_in("red",3);
+    digitalWrite(LED_BUILTIN, LOW);
+    fade_out("red",3);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(50);
+  }
+}
+
+// Open valve and turn on NeoPixels
 void turn_on() {
-  digitalWrite(LED_BUILTIN, LOW); // LED on
-  digitalWrite(valve_output, HIGH); // valve open
-  if (light_on==0) {
-    fade_in(strip.Color(  0,   0, 255), 5); 
+  if (valve_open == 0) {
+    timer_start = millis();           // start timer
+    digitalWrite(LED_BUILTIN, LOW);   // LED on
+    digitalWrite(valve_output, HIGH); // valve open
+    valve_open = 1;
+  }
+  if (light_on == 0) {
+    fade_in("blue", 5); 
+    light_on = 1;
+  }
+  // Report error if valve has been open for longer than the specified error_time
+  if (valve_open == 1) {
+    current_time = millis();
+    if (current_time - timer_start > error_time) {
+      error_status = 1;
+      error();
+    }
   }
   delay(50);
 }
 
-// Close valve and turn off LEDs
+// Close valve and turn off NeoPixels
 void turn_off() {
-  digitalWrite(LED_BUILTIN, HIGH); // LED off
-  digitalWrite(valve_output, LOW); // valve closed
-  if (light_on==1) {
-    fade_out(strip.Color(  0,   0, 255), 5); 
+  if (valve_open == 1) {
+    digitalWrite(LED_BUILTIN, HIGH); // LED off
+    digitalWrite(valve_output, LOW); // valve closed
+    valve_open = 0;
+  }
+  if (light_on == 1) {
+    fade_out("blue", 5);
+    light_on = 0;
   }
   delay(50);
 }
@@ -171,7 +224,7 @@ void loop() {
     }
   }
 
-  // object detected from IR sensor
+  // object detected
   while (ir_state == LOW) {
     delay(ir_input_delay);
     ir_state = digitalRead(ir_input);
@@ -183,5 +236,6 @@ void loop() {
     }
   }
   
-} //end loop()
+  delay(50);
+}
 
